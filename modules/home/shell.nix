d3049d16f35890f03ac093abe8dbb742f30d7a41
realdhiru <realdhiru00@gmail.com>
2026-06-08 14:30:01 +0530

    Add shell helpers

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -1,7 +1,38 @@
 { ... }:
 
 {
-  programs.zsh.enable = true;
+  programs.zsh = {
+    enable = true;
+
+    initContent = ''
+      rebuild() {
+        cd ~/nix || return
+
+        git add -A
+
+        if [ $# -eq 0 ]; then
+          git commit -m "Update configuration" || true
+        else
+          git commit -m "$*" || true
+        fi
+
+        sudo nixos-rebuild switch --flake .#vivobook
+      }
+
+      update() {
+        cd ~/nix || return
+
+        git pull
+        nix flake update
+
+        rebuild "flake update"
+      }
+
+      clean() {
+        sudo nix-collect-garbage -d
+      }
+    '';
+  };
 
   programs.starship = {
     enable = true;
