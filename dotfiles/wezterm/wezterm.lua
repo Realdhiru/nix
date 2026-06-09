84bc28dcdea3a9da278766d09c5ae43e0481938c
realdhiru <realdhiru00@gmail.com>
2026-06-09 19:14:21 +0530

    restore clean shell and wezterm

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -2,16 +2,11 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.scrollback_lines = 100000
-config.window_close_confirmation = "NeverPrompt"
-config.hide_tab_bar_if_only_one_tab = true
-config.adjust_window_size_when_changing_font_size = false
 
-wezterm.on("gui-startup", function()
-  wezterm.background_child_process({
-    "zsh",
-    "-ic",
-    "fastfetch"
-  })
-end)
+config.window_close_confirmation = "NeverPrompt"
+
+config.hide_tab_bar_if_only_one_tab = true
+
+config.adjust_window_size_when_changing_font_size = false
 
 return config
