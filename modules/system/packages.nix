80a02db35035582e0c9e7295d3a52461d9f79558
realdhiru <realdhiru00@gmail.com>
2026-06-10 18:40:12 +0530

    cleanup

--- modules/system/packages.nix
+++ modules/system/packages.nix
@@ -2,9 +2,8 @@
 
 {
   environment.systemPackages = with pkgs; [
-    #
+
     # CLI
-    #
     vim
     wget
     git
@@ -15,37 +14,24 @@
     weathr
     libva-utils
 
-    #
     # Development
-    #
     vscodium
 
-    #
     # Browsers
-    #
     inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
     brave
 
-    #
     # File Managers
-    #
     thunar
 
-    #
     # Launchers
-    #
     rofi
 
-    #
     # Clipboard
-    #
     wl-clipboard
     cliphist
-    nwg-clipman
 
-    #
     # Screenshots / Recording
-    #
     grim
     slurp
     grimblast
@@ -53,90 +39,48 @@
     gpu-screen-recorder
     gpu-screen-recorder-gtk
 
-    #
     # Media controls
-    #
     brightnessctl
     playerctl
 
-    #
     # Notifications
-    #
     libnotify
 
-    #
     # Search
-    #
     fsearch
 
-    #
     # Polkit
-    #
     polkit_gnome
 
-    #
     # Filesystems
-    #
     ntfs3g
 
-    #
     # Terminal
-    #
     wezterm
 
-    #
-    # Music
-    #
+    # Video / Audio
+    mpv
     spotify
 
-    #
-    # Video / Audio
-    #
-    mpv
-
-    #
     # Creative
-    #
     blender
     kdePackages.kdenlive
 
-    #
     # Documents
-    #
     kdePackages.okular
     onlyoffice-desktopeditors
 
-    #
     # Images
-    #
     loupe
     imv
 
-    #
-    # Theme tools
-    #
-    lxappearance
-    nwg-look
-    nwg-icon-picker
-
-    #
-    # Displays
-    #
-    nwg-displays
-
-    #
     # Downloads
-    #
     parabolic
 
-    #
     # Volume
-    #
     pwvucontrol
 
-    #
     # Intel VAAPI
-    #
     intel-media-driver
     vpl-gpu-rt
   ];
