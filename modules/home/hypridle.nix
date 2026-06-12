dca25b8ae786609c37b635cca2db5ccadeff950c
realdhiru <realdhiru00@gmail.com>
2026-06-13 01:19:49 +0530

    Update configuration

--- modules/home/hypridle.nix
+++ modules/home/hypridle.nix
@@ -4,7 +4,7 @@
 
     settings = {
       general = {
-        lock_cmd = "quickshell -p ~/.config/quickshell/Lock.qml";
+        lock_cmd = "quickshell -p ~/.config/hypr/scripts/quickshell/Lock.qml";
         before_sleep_cmd = "loginctl lock-session";
         after_sleep_cmd = "hyprctl dispatch dpms on";
       };
