f201edf3db3c173a270342738f45d5f8ec38d053
realdhiru <realdhiru00@gmail.com>
2026-06-14 11:55:09 +0530

    Update configuration

--- dotfiles/rofi/default.nix
+++ dotfiles/rofi/default.nix
@@ -1,5 +1,6 @@
 { config, lib, ... }:
 
-{ 
-  xdg.configFile."rofi/config.rasi".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/config/programs/rofi/config.rasi";
+{
+  xdg.configFile."rofi/config.rasi".source = config.lib.file.mkOutOfStoreSymlink "/home/realdhiru/nix/dotfiles/rofi/config.rasi";
+  xdg.configFile."rofi/theme.rasi".source = config.lib.file.mkOutOfStoreSymlink "/home/realdhiru/nix/dotfiles/rofi/theme.rasi";
 }
