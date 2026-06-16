eac929030ba45ad08c889b89d348a00fde3ef05d
realdhiru <realdhiru00@gmail.com>
2026-06-16 14:46:15 +0530

    Update configuration

--- dotfiles/hypr/scripts/set_wallpaper.sh
+++ dotfiles/hypr/scripts/set_wallpaper.sh
@@ -3,10 +3,13 @@
 WALL="$1"
 
 pkill mpvpaper 2>/dev/null
-
 sleep 0.1
 
 mpvpaper \
 -o "no-audio --loop-playlist --hwdec=auto --panscan=1.0" \
 '*' \
 "$WALL" &
+
+matugen image "$WALL" -c ~/nix/dotfiles/matugen/config.toml -m dark -t expressive
+
+touch ~/.config/wezterm/wezterm.lua
