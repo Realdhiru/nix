9a45c5ad2cbbc5453bbb1965deb2eb15425c26d9
realdhiru <realdhiru00@gmail.com>
2026-06-17 11:39:44 +0530

    Update configuration

--- hosts/vivobook/default.nix
+++ hosts/vivobook/default.nix
@@ -1,3 +1,5 @@
+{ config, pkgs, ... }:
+
 {
   imports = [
     ./hardware-configuration.nix
@@ -27,6 +29,14 @@
   programs.hyprland.enable = true;
 
   i18n.defaultLocale = "en_US.UTF-8";
+
+  # Prevent home-manager early activation mismatches from throwing boot warnings
+  systemd.services."home-manager-realdhiru" = {
+    serviceConfig = {
+      Restart = "on-failure";
+      RestartSec = "5s";
+    };
+  };
 
   system.stateVersion = "25.11";
 }
