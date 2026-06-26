7eb6787e601ed911b89d9a426a837b0d4d061610
realdhiru <realdhiru00@gmail.com>
2026-06-26 22:31:42 +0530

    focus

--- dotfiles/hypr/scripts/quickshell/focustime/launch_daemon.sh
+++ dotfiles/hypr/scripts/quickshell/focustime/launch_daemon.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 source "$(dirname "${BASH_SOURCE[0]}")/../../caching.sh"
-exec python3 "$(dirname "$0")/focus_daemon.py" 
+
+pkill -f focus_daemon.py
+
+while true; do
+    python3 "$(dirname "$0")/focus_daemon.py"
+    sleep 2
+done
