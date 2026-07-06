7498de3206c813ec3ed90472485694154aebf739
realdhiru <realdhiru00@gmail.com>
2026-07-06 14:45:52 +0530

    windowreg

--- dotfiles/hypr/scripts/quickshell/WindowRegistry.js
+++ dotfiles/hypr/scripts/quickshell/WindowRegistry.js
@@ -1,21 +1,42 @@
 .pragma library
 
+const REF_W = 1920.0;
+const REF_H = 1080.0;
+const MIN_SCALE = 0.35;
+const HIDE_OFFSET = -5000;
+
+const WIDGETS = {
+    "battery":   { w: 801,  h: 760, anchor: "top-right",     mx: 0, my: 60, mr: 4, mb: 0, path: "battery/BatteryPopup.qml" },
+    "network":   { w: 900,  h: 700, anchor: "top-right",     mx: 0, my: 60, mr: 4, mb: 0, path: "network/NetworkPopup.qml" },
+    "volume":    { w: 450,  h: 700, anchor: "top-right",     mx: 0, my: 60, mr: 5, mb: 0, path: "volume/VolumePopup.qml" },
+    "clipboard": { w: 800,  h: 700, anchor: "center",        mx: 0, my: 0,  mr: 0, mb: 0, path: "clipboard/ClipboardManager.qml" },
+    "monitors":  { w: 800,  h: 650, anchor: "center",        mx: 0, my: 0,  mr: 0, mb: 0, path: "monitors/MonitorPopup.qml" },
+    "stewart":   { w: 800,  h: 650, anchor: "center",        mx: 0, my: 0,  mr: 0, mb: 0, path: "stewart/stewart.qml" },
+    "focustime": { w: 900,  h: 700, anchor: "center",        mx: 0, my: 0,  mr: 0, mb: 0, path: "focustime/FocusTimePopup.qml" },
+    "guide":     { w: 1200, h: 750, anchor: "center",        mx: 0, my: 0,  mr: 0, mb: 0, path: "guide/GuidePopup.qml" },
+    "calendar":  { w: 1450, h: 750, anchor: "top-center",    mx: 0, my: 60, mr: 0, mb: 0, path: "calendar/CalendarPopup.qml" },
+    "updater":   { w: 950,  h: 850, anchor: "center",        mx: 0, my: 0,  mr: 0, mb: 0, path: "updater/UpdaterPopup.qml" },
+    "wallpaper": { w: -1,   h: 650, anchor: "center-fill",   mx: 0, my: 0,  mr: 0, mb: 0, path: "wallpaper/WallpaperPicker.qml" },
+    "music":     { w: 700,  h: 650, anchor: "top-left",      mx: 5, my: 60, mr: 0, mb: 0, path: "music/MusicPopup.qml" },
+    "movies":    { w: 1370, h: 850, anchor: "bottom-center", mx: 0, my: 0,  mr: 0, mb: 0, path: "movies/MovieWidget.qml" }
+};
+
 function getScale(mw, mh, userScale) {
-    if (arguments.length === 2) {
+    if (userScale === undefined && mh !== undefined) {
         userScale = mh;
-        mh = mw * (1080.0 / 1920.0);
+        mh = mw * (REF_H / REF_W);
     }
 
     if (mw <= 0 || mh <= 0) return 1.0;
     
-    let rw = mw / 1920.0;
-    let rh = mh / 1080.0;
+    let rw = mw / REF_W;
+    let rh = mh / REF_H;
     let r = Math.min(rw, rh);
     
     let baseScale = 1.0;
     
     if (r <= 1.0) {
-        baseScale = Math.max(0.35, Math.pow(r, 0.85));
+        baseScale = Math.max(MIN_SCALE, Math.pow(r, 0.85));
     } else {
         baseScale = Math.pow(r, 0.5);
     }
@@ -28,66 +49,70 @@ function s(val, scale) {
 }
 
 function getLayout(name, mx, my, mw, mh, userScale) {
-    let scale = getScale(mw, mh, userScale);
-    let t = null;
-
-    // Use a switch block to execute math and allocate an object ONLY for the requested widget
-    switch (name) {
-        case "battery":
-            t = { w: s(801, scale), h: s(760, scale), rx: mw - s(805, scale), ry: s(60, scale), comp: "battery/BatteryPopup.qml" };
-            break;
-        case "network":
-            t = { w: s(900, scale), h: s(700, scale), rx: mw - s(904, scale), ry: s(60, scale), comp: "network/NetworkPopup.qml" };
-            break;
-        case "volume":
-            t = { w: s(450, scale), h: s(700, scale), rx: mw - s(455, scale), ry: s(60, scale), comp: "volume/VolumePopup.qml" };
-            break;
-        case "clipboard":
-            t = { w: s(800, scale), h: s(700, scale), rx: Math.floor((mw/2)-(s(800, scale)/2)), ry: Math.floor((mh/2)-(s(700, scale)/2)), comp: "clipboard/ClipboardManager.qml" };
-            break;
-        case "monitors":
-            t = { w: s(800, scale), h: s(650, scale), rx: Math.floor((mw/2)-(s(800, scale)/2)), ry: Math.floor((mh/2)-(s(650, scale)/2)), comp: "monitors/MonitorPopup.qml" };
-            break;
-        case "stewart":
-            t = { w: s(800, scale), h: s(650, scale), rx: Math.floor((mw/2)-(s(800, scale)/2)), ry: Math.floor((mh/2)-(s(650, scale)/2)), comp: "stewart/stewart.qml" };
-            break;
-        case "focustime":
-            t = { w: s(900, scale), h: s(700, scale), rx: Math.floor((mw/2)-(s(900, scale)/2)), ry: Math.floor((mh/2)-(s(700, scale)/2)), comp: "focustime/FocusTimePopup.qml" };
-            break;
-        case "guide":
-            t = { w: s(1200, scale), h: s(750, scale), rx: Math.floor((mw/2)-(s(1200, scale)/2)), ry: Math.floor((mh/2)-(s(750, scale)/2)), comp: "guide/GuidePopup.qml" };
-            break;
-        case "calendar":
-            t = { w: s(1450, scale), h: s(750, scale), rx: Math.floor((mw/2)-(s(1450, scale)/2)), ry: s(60, scale), comp: "calendar/CalendarPopup.qml" };
-            break;
-        case "updater":
-            t = { w: s(950, scale), h: s(850, scale), rx: Math.floor((mw/2)-(s(950, scale)/2)), ry: Math.floor((mh/2)-(s(850, scale)/2)), comp: "updater/UpdaterPopup.qml" };
-            break;
-        case "wallpaper":
-            t = { w: mw, h: s(650, scale), rx: 0, ry: Math.floor((mh/2)-(s(650, scale)/2)), comp: "wallpaper/WallpaperPicker.qml" };
-            break;
-        case "music":
-            t = { w: s(700, scale), h: s(650, scale), rx: s(5, scale), ry: s(60, scale), comp: "music/MusicPopup.qml" };
-            break;
-        case "movies":
-            t = { w: s(1370, scale), h: s(850, scale), rx: Math.floor((mw / 2) - (s(1370, scale) / 2)), ry: mh - s(850, scale), comp: "movies/MovieWidget.qml" };
-            break;
-        case "hidden":
-            t = { w: 1, h: 1, rx: -5000 - mx, ry: -5000 - my, comp: "" };
-            break;
-        default:
-            return null;
+    if (name === "hidden") {
+        return { 
+            w: 1, 
+            h: 1, 
+            rx: HIDE_OFFSET - mx, 
+            ry: HIDE_OFFSET - my, 
+            comp: "",
+            x: HIDE_OFFSET,
+            y: HIDE_OFFSET
+        };
     }
 
-    t.x = mx + t.rx;
-    t.y = my + t.ry;
-    return t;
+    const config = WIDGETS[name];
+    if (!config) return null;
+
+    let scale = getScale(mw, mh, userScale);
+    let finalW = config.w === -1 ? mw : s(config.w, scale);
+    let finalH = s(config.h, scale);
+    
+    let rx = 0;
+    let ry = 0;
+
+    switch (config.anchor) {
+        case "top-left":
+            rx = s(config.mx, scale);
+            ry = s(config.my, scale);
+            break;
+        case "top-right":
+            rx = mw - finalW - s(config.mr, scale);
+            ry = s(config.my, scale);
+            break;
+        case "top-center":
+            rx = Math.floor((mw / 2) - (finalW / 2));
+            ry = s(config.my, scale);
+            break;
+        case "center":
+            rx = Math.floor((mw / 2) - (finalW / 2));
+            ry = Math.floor((mh / 2) - (finalH / 2));
+            break;
+        case "center-fill":
+            rx = 0;
+            ry = Math.floor((mh / 2) - (finalH / 2));
+            break;
+        case "bottom-center":
+            rx = Math.floor((mw / 2) - (finalW / 2));
+            ry = mh - finalH - s(config.mb, scale);
+            break;
+    }
+
+    return {
+        w: finalW,
+        h: finalH,
+        rx: rx,
+        ry: ry,
+        comp: config.path,
+        x: mx + rx,
+        y: my + ry
+    };
 }
 
 function getPopupLayout(mw, mh, userScale) {
-    if (arguments.length === 2) {
+    if (userScale === undefined && mh !== undefined) {
         userScale = mh;
-        mh = mw * (1080.0 / 1920.0);
+        mh = mw * (REF_H / REF_W);
     }
     
     let scale = getScale(mw, mh, userScale);
