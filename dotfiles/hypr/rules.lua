f6217dd19b6bed668e503ed989e00d10625dbed0
realdhiru <realdhiru00@gmail.com>
2026-08-24 02:11:23 +0530

    style(hyprland): remove wezterm window rule for opacity

--- dotfiles/hypr/rules.lua
+++ dotfiles/hypr/rules.lua
@@ -2,7 +2,6 @@
 -- Opacity
 -- ======================================================
 
-hl.window_rule({ match = { class = "^org\\.wezfurlong\\.wezterm$" }, opacity = "0.52" })
 hl.window_rule({ match = { class = "^codium$" }, opacity = "0.63" })
 hl.window_rule({ match = { class = "^antigravity-ide$" }, opacity = "0.71" })
 hl.window_rule({ match = { class = "^(pcmanfm-qt)$" }, opacity = "0.6" })
