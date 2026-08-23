857a4b1086f153d6568a612fafbfb1097d1980b5
realdhiru <realdhiru00@gmail.com>
2026-08-24 02:11:23 +0530

    style(wezterm): set window background opacity natively to 0.5

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -6,6 +6,8 @@ config.window_close_confirmation = "NeverPrompt"
 config.hide_tab_bar_if_only_one_tab = true
 config.adjust_window_size_when_changing_font_size = false
 
+config.window_background_opacity = 0.5
+
 -- Render via WebGpu (Vulkan/ANV): the iris-GL path on mesa 26.2 + i915 hangs
 -- (ecode 12:1:859ffffb) on this Alder Lake Iris Xe; the Vulkan path is clean.
 config.front_end = "WebGpu"
